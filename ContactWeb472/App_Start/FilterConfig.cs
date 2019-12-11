98b41f977361bfb38fdb688b7fb28c3ea311bf69
shayaansiddiqui <shayaan.siddiqui@gmail.com>
2019-12-11 17:15:05 -0600

    Initial Commit

---
+++ ContactWeb472/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ContactWeb472
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ContactWeb472/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContactWeb472.Startup))]
+namespace ContactWeb472
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
